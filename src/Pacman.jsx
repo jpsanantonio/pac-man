93e2c12a77b4181e792bf1e2a9751afca0ef073c
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-08-15 20:50:00 +0000

    Define keyboard shortcuts

--- src/Pacman.jsx
+++ src/Pacman.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useRef } from "react";
 
-function Pacman() {
-  const canvasRef = useRef(null);
-
+function useCircleDrawer(canvasRef) {
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
@@ -15,7 +13,43 @@ function Pacman() {
     context.arc(x, y, radius, 0, Math.PI * 2, false);
     context.closePath();
     context.fill();
-  }, []);
+  }, [canvasRef]);
+}
+
+function useKeyboardShortcut() {
+  const handleKeydown = ({ code }) => {
+    switch (code) {
+      case "ArrowUp":
+        console.log("up");
+        break;
+      case "ArrowDown":
+        console.log("down");
+        break;
+      case "ArrowLeft":
+        console.log("left");
+        break;
+      case "ArrowRight":
+        console.log("right");
+        break;
+      default:
+        return;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeydown);
+    };
+  });
+}
+
+function Pacman() {
+  const canvasRef = useRef(null);
+
+  useKeyboardShortcut();
+  useCircleDrawer(canvasRef);
 
   return (
     <>
