410d0d1cf63f867689c0f1612f46f34526225b92
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-08-15 20:19:37 +0000

    Draw a circle

--- src/Pacman.jsx
+++ src/Pacman.jsx
@@ -1,7 +1,21 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function Pacman() {
   const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    const x = 50;
+    const y = 100;
+    const radius = 20;
+
+    context.fillStyle = "#000000";
+    context.beginPath();
+    context.arc(x, y, radius, 0, Math.PI * 2, false);
+    context.closePath();
+    context.fill();
+  }, []);
 
   return (
     <>
